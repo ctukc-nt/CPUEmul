227d157f853787c8c5264bf16377404e6f77f10e
ctukc <ctukc.nt@gmail.com>
2015-05-15 21:06:37 +0300

    Набивка главной формы

--- CPUEmul/MainForm.cs
+++ CPUEmul/MainForm.cs
@@ -16,5 +16,20 @@ namespace CPUEmul
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
